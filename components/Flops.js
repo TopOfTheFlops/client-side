57629e6b7eaf6bc5e8412ef4fba474f803035836
gabrielamelian <gabrielamelian@gmail.com>
2016-12-09 14:20:41 +1300

    flops template working

--- components/Flops.js
+++ components/Flops.js
@@ -7,6 +7,7 @@ function Flops({state}) {
   return (
     <div>
       <Header />
+      {RenderTitle(state)}
       {RenderFlops(state)}
       <button className='create'>Compete!</button>
       <Nav />
@@ -16,18 +17,29 @@ function Flops({state}) {
 
 function RenderFlops(state) {
   return state.flops
-    .filter( flop =>  flop.lifestyleId == state.currentLifestyleId)
-    .map(flop => {
+  .sort((a, b) => b.upvotes - a.upvotes)
+    .filter(flop => {
+      return flop.lifestyleId == state.currentLifestyleId
+    })
+    .map( (flop, index) => {
+      flop.rank = index+1
       return (
         <div className='lifestyle'>
-          <h3>{flop.name}</h3>
-          <img className='flopPic' src={flop.media}/>
+        <img className='flopPic' src={flop.media}/>
+          <h3>{flop.rank} {flop.name}</h3>
           <p>{flop.description}</p>
           <button>cool!</button>
+          <p>up: {flop.upvotes}</p>
           <button>stupid!</button>
+          <p>down: {flop.downvotes}</p>
         </div>
       )
     })
+}
+
+function RenderTitle(state) {
+  const {lifestyles, currentLifestyleId} = state
+  return lifestyles.filter( lifestyle => lifestyle.lifestyleId == currentLifestyleId).map(lifestyle => (<h2>{lifestyle.title}</h2>))
 }
 
 module.exports = Flops
