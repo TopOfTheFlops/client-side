63728b73a248594d02b67fc9c9fb019b1d915761
gabrielamelian <gabrielamelian@gmail.com>
2016-12-11 13:25:34 +1300

    Add with credentials to downvote

--- api/downvoteFlop.js
+++ api/downvoteFlop.js
@@ -3,6 +3,7 @@ import request from 'superagent'
 module.exports = (dispatch, flopId) => {
   request
     .post('https://topoftheflops.herokuapp.com/api/v1/flops/vote')
+    .withCredentials()
     .send({
       action: "downvote",
       flopId: flopId
