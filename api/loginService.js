4c38613aaf6cc36c15b46af04221309002bb0624
gabrielamelian <gabrielamelian@gmail.com>
2016-12-11 13:22:39 +1300

    Adds with credentials to login request

--- api/loginService.js
+++ api/loginService.js
@@ -11,6 +11,7 @@ module.exports = (dispatch, userInfo) => {
   request
     .post('https://topoftheflops.herokuapp.com/api/v1/users/login')
     .send(userInfo)
+    .withCredentials()
     .end((err, res) => {
       if (err) {
         dispatch({type: 'LOGIN_UNSUCCESSFUL'})
