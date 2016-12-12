ee5adefd01c17edd3bc35c541ea8de07ca6497f5
Kate Norquay <katenorquay@live.com>
2016-12-12 21:51:34 +1300

    testing api for image upload

--- components/CreateFlop.js
+++ components/CreateFlop.js
@@ -1,11 +1,24 @@
 import React from 'react'
+var imgur = require('imgur');
+var unirest = require('unirest')
 
 import Header from './Header'
 import Nav from './Nav'
 
 import postNewFlop from '../api/postNewFlop'
 
+
 function CreateFlop({state, dispatch}) {
+
+  function callAPI() {
+    unirest.post('https://imgur-apiv3.p.mashape.com/3/image')
+    .header("X-Mashape-Key", 'AwnXEjf7R6mshadV7LQK5RcyinuJp1HUqeUjsnyJAsuYyEuEzL')
+    .header("Authorization", "Client-ID 9c34c3606f727ce")
+    .attach("image", `${document.getElementById('mediaURL').files[0]}`)
+    .end(function (result) {
+      console.log(result)
+    })
+  }
 
   function createNewFlop (e) {
     console.log('Creating new flop');
@@ -25,10 +38,10 @@ function CreateFlop({state, dispatch}) {
       <h2>Flopping</h2>
       {RenderTitle(state)}
       <form>
-        <input placeholder='Image url' type="text" id='mediaURL'/>
+        <input placeholder='Image url' type="file" id='mediaURL'/>
         <label>Describe your entry</label>
         <input placeholder='Description' type="text" id='description'/>
-        <input className='loginButton' type='submit' value='compete!' onClick={createNewFlop}/>
+        <input className='loginButton' type='submit' value='compete!' onClick={callAPI}/>
       </form>
       <Nav dispatch={dispatch} state={state}/>
     </div>
